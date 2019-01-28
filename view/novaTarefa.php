4b9b42d55472a8e637eb4a100e85b47d90c7d4af
Fábio Krakauer <comvocefabio@gmail.com>
2019-01-28 16:28:01 -0200

    add newAssigment file

---
+++ view/novaTarefa.php
@@ -0,0 +1,10 @@
+<?php
+
+$dir = realpath(__DIR__ . '/..');
+require_once $dir.'/config/config.php';
+
+include APP_ROOT . "/include/header.php";
+
+?>
+
+<?php include APP_ROOT . "/include/footer.php";

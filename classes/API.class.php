390b3b25cef9c05240e868fa6aa0722348d27b83
Fábio Krakauer <comvocefabio@gmail.com>
2019-01-28 17:05:02 -0200

    add newAssigment

--- classes/API.class.php
+++ classes/API.class.php
@@ -6,4 +6,26 @@ class API{
     public static function categoryName(){
         return ["1" => "Lição de casa", "2" => "Prova", "3" => "Atividade"];
     }
+    public static function getMatters(){
+        return [
+            "1" => "Matematica",
+            "2" => "História",
+            "3" => "Geografia",
+            "4" => "Educação Fisica",
+            "5" => "Inglês",
+            "6" => "Espanhol",
+            "7" => "Religião",
+            "8" => "Sociologia",
+            "9" => "Física",
+            "10" => "Química",
+            "11" => "Biologia",
+            "12" => "Filosofia",
+            "13" => "Literatura",
+            "14" => "Gramática",
+            "15" => "Redação"
+        ];
+    }
+    public static function getMatterByID($id){
+        return API::getMatters()[$id];
+    }
 }
--- view/novaTarefa.php
+++ view/novaTarefa.php
@@ -2,6 +2,7 @@
 
 $dir = realpath(__DIR__ . '/..');
 require_once $dir.'/config/config.php';
+require_once $dir.'/classes/API.class.php';
 
 include APP_ROOT . "/include/header.php";
 ?>
@@ -10,7 +11,41 @@ include APP_ROOT . "/include/header.php";
     <h3 class="text-center mt-3">Cadastrar nova tarefa</h3>
     <form action="/calendario/controller/newAssigment.php" method="post">
         <div class="row">
-            
+            <div class="form-group col-12 col-md-4">
+                <label for="category">Selecione o tipo</label>
+                <select name="category" class="form-control" id="category">
+                    <?php 
+                        foreach(API::categoryName() as $id=>$category){
+                            echo '<option value="'.$id.'">'.$category.'</option>';
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="form-group col-12 col-md-4">
+                <label for="matter">Selecione a materia</label>
+                <select name="matter" id="matter" class="form-control">
+                    <?php 
+                        foreach(API::getMatters() as $id=>$matter){
+                            echo '<option value="'.$id.'">'.$matter.'</option>';
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="form-group col-12 col-md-4">
+                <label for="date">Selecionar data de entrega</label>
+                <input type="date" name="date" class="form-control">
+            </div>
+        </div>
+        <div class="row">
+            <div class="form-group col-12">
+                <label for="description">Digite a descrição</label>
+                <textarea name="description" id="description" cols="30" placeholder="Descrição" class="form-control"></textarea>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="form-group col-12">
+                <button type="submit" name="action" class="btn btn-success col-12" value="action"><i class="fa fa-arrow-circle-up" aria-hidden="true"></i> Cadastrar!</button>
+            </div>
         </div>
     </form>
 

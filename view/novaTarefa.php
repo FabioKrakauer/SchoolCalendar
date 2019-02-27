627b69c9ba3afa52f464d441bb3cd29276e1bda5
Fábio Krakauer <comvocefabio@gmail.com>
2019-02-27 19:22:05 -0300

    add input to new File

--- view/novaTarefa.php
+++ view/novaTarefa.php
@@ -7,10 +7,21 @@ require_once $dir.'/classes/Auth.class.php';
 
 Auth::isLogged(true);   
 include APP_ROOT . "/include/header.php";
+if(!isset($_SESSION)){
+    session_start();
+}
 ?>
 
 <div class="container mt-2">
     <h3 class="text-center mt-3">Cadastrar nova tarefa</h3>
+    <?php
+        if(isset($_SESSION["newAssigmentError"])){
+            echo '<div class="alert alert-danger" role="alert">
+                    <strong>'. $_SESSION["newAssigmentError"] . '</strong>
+                  </div>';
+        }
+    ?>
+    
     <form action="/calendario/controller/newAssigment.php" method="post">
         <div class="row">
             <div class="form-group col-12 col-md-4">
@@ -41,7 +52,16 @@ include APP_ROOT . "/include/header.php";
         <div class="row">
             <div class="form-group col-12">
                 <label for="description">Digite a descrição</label>
-                <textarea name="description" id="description" cols="30" placeholder="Descrição" class="form-control"></textarea>
+                <textarea name="description" id="description" rows="5" placeholder="Descrição" class="form-control"></textarea>
+            </div>
+        </div>
+        <div class="row">
+            <div class="form-group col-12">
+                <label for="description">Anexar arquivo</label>
+                <input type="file" name="file" id="file" class="file-input-control" onchange="onChangeInput()">
+                <label for="file" class="btn btn-outline-primary form-control mt-3" id="button-newFile">
+                    <i class="fa fa-upload" aria-hidden="true"></i> <label for="file" id="label-File">Carregar arquivo</label>
+                </label>
             </div>
         </div>
         <div class="row justify-content-center">
